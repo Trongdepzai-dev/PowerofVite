b450ccc12333ee4aa3d1422bfb9d567616f7ccd5
B.Trọng <trongha644@gmail.com>
2025-05-05 22:47:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,20 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    // Đảm bảo dòng 'base' này được thiết lập đúng
+    // Sử dụng tên repository của bạn: PowerofVite
+    base: '/PowerofVite/',
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
